dad737a85bd4e709a24065cd76e03eaa40d4c2c3
Lenard9999 <37104220+Lenard9999@users.noreply.github.com>
2021-01-27 15:32:10 +0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 
 public class App {
+    public static String des;
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         int mode = 0;
@@ -18,10 +19,10 @@ public class App {
                 case 1: subnetCalc();
                         break;
 
-                case 2: //checkClass();
+                case 2: checkClass();
                         break;
 
-                case 3: //checkType();
+                case 3: checkType();
                         break;
                 
                 case 4: break;
@@ -44,10 +45,10 @@ public class App {
         networkNum = scanner.nextInt();
 
         while(ctr != networkNum){
-            System.out.println("Input the name of network " + (ctr+1) + ": ");
+            System.out.print("Input the name of network " + (ctr+1) + ": ");
             scanner.nextLine();
             String tempName = scanner.nextLine();
-            System.out.println("Input the number of IP Adresses needed: ");
+            System.out.print("Input the number of IP Adresses needed: ");
             Integer tempNum = scanner.nextInt();
             network.add(new Network(tempName, tempNum));
 
@@ -83,7 +84,7 @@ public class App {
 
         System.out.println("\n");
         System.out.println("Address Information");
-        System.out.println("ID\tFirst Usable Addr\tLast Usable Addr\tUsable IPs\t Free IPs");
+        System.out.println("ID\tFirst Usable Addr\tLast Usable Addr\tBroadcast Address\tUsable IPs\t Free IPs");
 
         networkAdd = ipAddress.getIP();
 
@@ -92,8 +93,11 @@ public class App {
             String firstUsable = calcFirst(networkAdd);
             String lastUsable = calcLast(networkAdd, CIDR);
             String broadCast = calcBroad(networkAdd, CIDR);
+            int usable = ((int) Math.pow(2, log2(network.get(ctr).netNum + 2))) - 2;
+            int freeIPs = usable - network.get(ctr).netNum;
 
-            System.out.println((ctr+1) + "\t" + firstUsable + "\t" + lastUsable + "\t" + broadCast);
+
+            System.out.println((ctr+1) + "\t" + firstUsable + "\t" + lastUsable + "\t" + broadCast + "\t" + usable + "\t" + freeIPs);
 
             networkAdd = calcNetwork(networkAdd, CIDR);
             ctr++;
@@ -237,13 +241,10 @@ public class App {
                 octets[x] = 0 + octets[x];
             }
             tempPlus += octets[x];        
-            System.out.println(octets[x]);
         }
-        System.out.println(tempPlus);
         
         result = addBinary(tempPlus, "1");
 
-        
         result = insertString(result, ".", 7);
         result = insertString(result, ".", 16);
         result = insertString(result, ".", 25);
@@ -345,6 +346,183 @@ public class App {
 
         return finalIP;
 
+    }
+
+    public static void checkClass(){
+        System.out.print("Input IP Address: ");
+        String ip_add = scanner.next();
+        String[] new_add = ip_add.split("\\."); 
+
+        int first = Integer.parseInt(new_add[0]); 
+        int second = Integer.parseInt(new_add[1]); 
+        int third = Integer.parseInt(new_add[2]);
+        int fourth = Integer.parseInt(new_add[3]);
+
+        des = "";
+
+        if((first < 0 || first > 255) || (second < 0 && second > 255) || (third < 0 && third > 255) || (fourth < 0 && fourth > 255)){
+            System.out.println("Error: Invalid IP Address.");
+        }
+        else if(SpecialIpAddress(first, second, third, fourth)){
+            System.out.println("The IP Address " + ip_add + " is a Special Purpose Address. " + des);
+        }
+        else if(first >= 0 && first <= 127){
+            System.out.println("The IP Address " + ip_add + " is a Class A Address, whose network address is " + first + ".0.0.0/8");
+        }
+        else if(first >= 128 && first <= 191){
+            System.out.println("The IP Address " + ip_add + " is a Class B Address, whose network address is " + first + "." + second + ".0.0/16");
+        }
+        else if(first >= 192 && first <= 223){
+            System.out.println("The IP Address " + ip_add + " is a Class C Address, whose network address is " + first + "." + second + "." + third + ".0/24");
+        }
+    }
+
+    public static boolean SpecialIpAddress(int first, int second, int third, int fourth){
+        if(first == 0){
+            des = "Current network only valid as source address. Address Block = 0.0.0.0/8";
+            return true;
+        }
+        else if(first == 100 && second >= 64 && second <= 127){
+            des = "Shared address space for communications between a service provider and its subscribers when using a carrier-grade NAT. Address Block = 100.64.0.0/10";
+            return true;
+        }   
+        else if(first == 127){
+            des = "Used for loopback addresses to the local host. Address Block = 127.0.0.0/8";
+            return true;
+        }
+        else if(first == 169 && second == 254){
+            des = "Used for link-local addresses between two hosts on a single link when no IP address is otherwise specified, such as would have normally been retrieved from a DHCP server. Address Block = 169.254.0.0/16";
+            return true;
+        }
+        else if(first == 192 && second == 0 && third == 0){
+            des = "IETF Protocol Assignments. Address Block = 192.0.0.0/24";
+            return true;
+        }
+        else if(first == 192 && second == 0 && third == 2){
+            des = "Assigned as TEST-NET-1, documentation and examples. Address Block = 192.0.2.0/24";
+            return true;
+        }
+        else if(first == 192 && second == 88 && third == 99){
+            des = "Reserved. Formerly used for IPv6 to IPv4 relay. Address Block = 192.88.99.0/24";
+            return true;
+        }
+        else if(first == 198 && (second == 18 || second == 19)){
+            des = "Used for benchmark testing of inter-network communications between two separate subnets. Address Block = 198.18.0.0/15";
+            return true;
+        }
+        else if(first == 198 && second == 51 && third == 100){
+            des = "Assigned as TEST-NET-2, documentation and examples. Address Block = 198.51.100.0/24";
+            return true;
+        }
+        else if(first == 203 && second == 0 && third == 113){
+            des = " Assigned as TEST-NET-3, documentation and examples. Address Block = 203.0.113.0/24";
+            return true;
+        }
+        else if(first >= 224 && first <= 239){
+            des = "In use for IP multicast. (Former Class D network). Address Block = 224.0.0.0/4";
+            return true;
+        }
+        else if(first == 255 && first == second && second == third && third == fourth){
+            des = "Reserved for future use. (Former Class E network). Address Block = 240.0.0.0/4";
+            return true;
+        }
+        else if(first >= 240 && first <= 255){
+            des = "Reserved for the limited broadcast destination address. Address Block = 255.255.255.255/32";
+            return true;
+        }
+
+        return false;
+    }
+    
+    public static void checkType(){
+        System.out.print("Input IP Address: ");
+        String ipadd = scanner.next();
+        String[] new_add = ipadd.split("/");
+        String[] address = new_add[0].split("\\."); 
+
+        int second = Integer.parseInt(address[1]); 
+        int third = Integer.parseInt(address[2]);
+        int fourth = Integer.parseInt(address[3]);
+        int fifth = Integer.parseInt(new_add[1]);
+
+        int newfifth = 0;
+
+        if(fifth >= 8 && fifth <= 15){
+            newfifth = (int) Math. pow(2,(16 - fifth));
+
+            if(fourth != 255){
+                if(fourth % newfifth == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Network Address");
+                }
+                else if(fourth % (newfifth-1) == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Broadcast Address");
+                }
+                else{
+                    System.out.println("The IP Address " + ipadd + " is a Host Address");
+                }
+            }
+            else if(third != 255){
+                if(third % newfifth == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Network Address");
+                }
+                else if(third % (newfifth-1) == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Broadcast Address");
+                }
+                else{
+                    System.out.println("The IP Address " + ipadd + " is a Host Address");
+                }
+            }
+            else{
+                if(second % newfifth == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Network Address");
+                }
+                else if(second % (newfifth-1) == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Broadcast Address");
+                }
+                else{
+                    System.out.println("The IP Address " + ipadd + " is a Host Address");
+                }
+            }
+        }
+        else if(fifth >= 16 && fifth <= 23){
+            newfifth = (int) Math. pow(2,(24 - fifth));
+
+            if(fourth != 255){
+                if(fourth % newfifth == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Network Address");
+                }
+                else if(fourth % (newfifth-1) == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Broadcast Address");
+                }
+                else{
+                    System.out.println("The IP Address " + ipadd + " is a Host Address");
+                }
+            }
+            else{
+                if(third % newfifth == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Network Address");
+                }
+                else if(third % (newfifth-1) == 0){
+                    System.out.println("The IP Address " + ipadd + " is a Broadcast Address");
+                }
+                else{
+                    System.out.println("The IP Address " + ipadd + " is a Host Address");
+                }
+            }
+        }
+        else{
+            newfifth = (int) Math. pow(2,(32 - fifth));
+
+            if(fourth % newfifth == 0){
+                System.out.println("The IP Address " + ipadd + " is a Network Address");
+            }
+            else if(fourth % (newfifth-1) == 0){
+                System.out.println("The IP Address " + ipadd + " is a Broadcast Address");
+            }
+            else{
+                System.out.println("The IP Address " + ipadd + " is a Host Address");
+            }
+        }
     }
 
 }
